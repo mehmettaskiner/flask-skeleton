98144d774b2c957e340922ef543d754b725493a9
Mehmet Taskiner <taskinermehmet@gmail.com>
2015-12-28 06:46:20 +0200

    simple json endpoint

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "hello devops, this is your home."
+  return 'hello devops, this is your home. <br><a href="./endpoint">click me</a>'
+
+
+@app.route("/endpoint")
+def endpoint():
+  devops = {
+    'name': 'Mehmet Taskiner', 
+    'github': 'http://github.com/mehmettaskiner'
+  }
+  return jsonify(devops)
 
 
 if __name__ == "__main__":
